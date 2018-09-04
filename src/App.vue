fd9278005498755cb5a1482e97c1ee715e6d1760
Snehanshu Phukon <snehanshuphukan@gmail.com>
2018-09-04 21:12:27 +0530

    Created basic indicator selector

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <indicator />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Indicator from '@/components/indicators'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  components: { Indicator }
 }
 </script>
 
---
+++ src/components/indicators.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <!--Indicator-->
+        <select v-model="selectedIndicator">
+            <option :value="null">INDICATOR</option>
+            <option :value="i" v-for="(indicator, i) in indicators" :key="'indicator-'+i">
+                {{ indicator.name }}
+            </option>
+        </select>
+
+        <!--Inputs-->
+        <template v-if="selectedIndicator != null">
+            <input type="number" :placeholder="input.name" :title="input.name"
+                v-for="(input, k) in indicators[selectedIndicator].inputs"
+                :key="'input-'+k" :value="input.dflt"
+            >
+        </template>
+
+        <!--Timeframe-->
+        <select v-model="timeframe" v-if="selectedIndicator != null">
+            <option :value="j" v-for="(tf, j) in timeframes" :key="'tf-'+j">
+                {{ tf.name }}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+import indicators from '@/indicators'
+
+export default {
+    data: () => ({
+        indicators,
+        selectedIndicator: null,
+        timeframe: 1,
+        timeframes: [
+            {
+                name: '1 min',
+                code: 'T1',
+            },
+            {
+                name: '5 min',
+                code: 'T5',
+            },
+            {
+                name: '10 min',
+                code: 'T10',
+            },
+            {
+                name: '30 min',
+                code: 'T30',
+            },
+            {
+                name: '60 min',
+                code: 'T60',
+            },
+        ],
+    }),
+}
+</script>
+
