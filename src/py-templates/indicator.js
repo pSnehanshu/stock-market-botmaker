08a45620d82afe78331840cbb2d0aeacd8eec8a9
Snehanshu Phukon <snehanshuphukan@gmail.com>
2018-09-05 16:25:11 +0530

    Fixed extra comma bug

--- src/py-templates/indicator.js
+++ src/py-templates/indicator.js
@@ -1,1 +1,12 @@
-export default `<%- fn %>( <% var i=0; for (k in ohlcKeys){%><%=( i>0? ',':'' )%><%-timeframe%>['<%- ohlcKeys[k] %>']<% i++; }%><% for (inp in inputs) {%>, <%-inp%>=<%-inputs[inp]%><%}%>)`
+export default `<%- fn %>(<% 
+    var i=0;
+    for (k in ohlcKeys) {
+        %><%- ( i>0? ',':'' ) %><%- timeframe %>['<%- ohlcKeys[k] %>']<% 
+        i++;
+    }
+    
+    j = 0;
+    for (inp in inputs) {
+        %><%- j==0? ( i>0? ',':'' ): ',' %> <%- inp %>=<%- inputs[inp] %><%
+        j++;
+    } %>)`
