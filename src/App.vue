b93f51ed78aee84b48fc94c032c1d3c9ca52d83c
Snehanshu Phukon <snehanshuphukan@gmail.com>
2018-09-05 22:37:52 +0530

    Implemented sl_tp_tk

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,42 @@
           <condition-list type="close" @change="close=$event" />
           <pre>{{close}}</pre>
       </div>
+      <div>
+          <h1>Stop loss</h1>
+          <sl-tp-tk @change="stopLoss='sl = '+$event" />
+      </div>
+      <div>
+          <h1>Take profit</h1>
+          <sl-tp-tk @change="takeProfit='tp = '+$event" />
+      </div>
+      <div>
+          <h1>Trailing Tick</h1>
+          <sl-tp-tk @change="tick='tk = '+$event" />
+      </div>
   </div>
 </template>
 
 <script>
 import ConditionList from '@/components/conditionList'
+import SlTpTk from '@/components/sl_tp_tk'
+import ejs from 'ejs'
 
 export default {
   name: 'app',
-  components: { ConditionList },
+  components: { ConditionList, SlTpTk },
   data: () => ({
     open: '',
     close: '',
+    stopLoss: '',
+    takeProfit: '',
+    tick: '',
   }),
   
+  computed: {
+    code() {
+      // Return full code
+    }
+  },
+
 }
 </script>
---
+++ src/components/sl_tp_tk.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <indicator @change="indicator=$event;handleChange()" />
+        <input type="text" v-model="modifier" placeholder="* 13 + 5">
+    </div>
+</template>
+
+<script>
+import Indicator from '@/components/indicators'
+
+export default {
+    components: { Indicator },
+    data: () => ({
+        modifier: '',
+        indicator: '',
+    }),
+    methods: {
+        handleChange() {
+            var code = this.indicator + this.modifier
+            this.$emit('change', code)
+        }
+    },
+    watch: {
+        modifier() {
+            this.handleChange()
+        },
+    },
+}
+</script>
