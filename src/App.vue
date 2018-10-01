b886217e88c2bd7d13500818f41e11854f47bb81
Snehanshu Phukon <snehanshuphukan@gmail.com>
2018-10-02 02:42:16 +0530

    Extracted config vars into seperate file

--- src/App.vue
+++ src/App.vue
@@ -42,6 +42,7 @@
 <script>
 import ConditionList from '@/components/conditionList'
 import SlTpTk from '@/components/sl_tp_tk'
+import config from '@/config.js'
 import ejs from 'ejs'
 import { saveAs } from 'file-saver/FileSaver'
 import AnalysisTmplt from '@/py-templates/analysis'
@@ -61,37 +62,13 @@ export default {
     stopLoss: '',
     takeProfit: '',
     tick: '',
-    configs: {
-        max_trades: {
-            name: 'Max trades',
-            val: 5,
-        },
-        qty_multiplier: {
-            name: 'Leverage',
-            val: 20,
-        },
-        qty_parts: {
-            name: 'Capital division',
-            val: 10,
-        },
-        trade_start_hour: {
-            name: 'Trade start hour',
-            val: 9,
-        },
-        trade_start_minute: {
-            name: 'Trade start minute',
-            val: 15,
-        },
-        trade_end_hour: {
-            name: 'Trade end hour',
-            val: 10,
-        },
-        trade_end_minute: {
-            name: 'Trade end minute',
-            val: 0,
-        },
-    },
   }),
+
+  computed: {
+      configs() {
+          return config
+      }
+  },
   
   methods: {
     getFile(){
---
+++ src/config.js
@@ -0,0 +1,30 @@
+export default {
+    max_trades: {
+        name: 'Max trades',
+        val: 5,
+    },
+    qty_multiplier: {
+        name: 'Leverage',
+        val: 20,
+    },
+    qty_parts: {
+        name: 'Capital division',
+        val: 10,
+    },
+    trade_start_hour: {
+        name: 'Trade start hour',
+        val: 9,
+    },
+    trade_start_minute: {
+        name: 'Trade start minute',
+        val: 15,
+    },
+    trade_end_hour: {
+        name: 'Trade end hour',
+        val: 10,
+    },
+    trade_end_minute: {
+        name: 'Trade end minute',
+        val: 0,
+    },
+}
